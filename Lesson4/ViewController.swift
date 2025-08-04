2eb16561ef756feb49e6afab5e529d8089084dc6
Vlad <lokotuin12@mail.ru>
2025-08-04 11:49:50 +0300

    Setup view controller

--- Lesson4/ViewController.swift
+++ Lesson4/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - IBOutlets
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        setupSlider()
+        setupMainLabel()
     }
-
-
+    
+    // MARK: - IBActions
+    @IBAction func sigmentedControlAction() {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "The first segmen is seleced"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "The second segmen is seleced"
+            mainLabel.textColor = .blue
+        default:
+            mainLabel.text = "The third segmen is seleced"
+            mainLabel.textColor = .yellow
+        }
+    }
+    
+    @IBAction func sliderAction() {
+        mainLabel.text = slider.value.formatted()
+        view.backgroundColor = view.backgroundColor?.withAlphaComponent(CGFloat(slider.value))
+    }
+    
+    @IBAction func doneButtonPassed() {
+        mainLabel.text = textField.text
+    }
+     
+    // MARK: - Private Methods
+    private func setupMainLabel() {
+        mainLabel.text = slider.value.formatted()
+        mainLabel.font = UIFont.systemFont(ofSize: 35)
+        mainLabel.textAlignment = .center
+        mainLabel.numberOfLines = 2
+    }
+    
+    private func setupSlider() {
+        slider.value = 1
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .red
+        slider.thumbTintColor = .blue
+    }
 }
 
